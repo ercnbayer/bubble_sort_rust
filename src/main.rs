b2e32c9190235e5aff8a40d915de16d9c3b11d62
EMRE <emre.bayer@hotmail.com>
2022-03-17 13:13:19 +0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,38 @@
-fn  bubble_sort(  mut vec: Vec<u32>)->Vec<u32>
+fn  bubble_sort(mut to_be_sorted: Vec<u32>)->Vec<u32>  
 {
-    
-    for step in  0..vec.len(){
+   for step in  0..to_be_sorted.len(){   
       let mut swapped=0;
-   for i in  0..(vec.len()-1-step)
+      for i in  0..(to_be_sorted.len()-1-step)
    {
 
-if vec[i]>vec[i+1]
-{
-let temp=vec[i];
-vec[i]=vec[i+1];
-vec[i+1]=temp;
-swapped=1;
-}
-
-
-
-
-   }
-   if swapped==0
+      if to_be_sorted[i]>to_be_sorted[i+1]
+      {
+         let temp=to_be_sorted[i];
+         to_be_sorted[i]=to_be_sorted[i+1];
+         to_be_sorted[i+1]=temp;
+         swapped=1;  
+      }
+   }                  
+      if swapped==0
    {
        break;
-}
-   }
-   
-  vec 
-}
-
+               }
+               }
+   to_be_sorted 
+} 
 fn main() {
-    let mut vec=vec![13,2,33,4,5,7,36,8,76,32,65,39];
-vec=bubble_sort(vec);
-for item in vec
-{ println!("{}",item);
- }
+    let mut to_be_sorted=vec![13,2,33,4,5,7,36,8,76,32,65,39];
+   to_be_sorted=bubble_sort(to_be_sorted);
+   for item in to_be_sorted
+{ 
+   println!("{}",item);
+                  
+}
     }
+
+  
+   
+ 
+
+
+
